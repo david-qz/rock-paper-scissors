db2df333cb033073e9d364e62433368bd0517f52
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-07 09:35:32 -0700

    Test and implement rock, paper, scissors game logic.
    Not sure what the best way to mimc an enum type is in JS, but this
    seems to work. I just want to move away from using magic numbers and
    string literals everywhere.

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,49 @@
-// IMPORT code to test here
-// import { fn } from '../module.js';
+import {
+    throws,
+    results,
+    getRandomThrow,
+    scoreRound,
+} from '../utils.js';
 
 const test = QUnit.test;
 
-test('test your pure functions...', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('rock paper scissors scoring', (expect) => {
+    expect.equal(scoreRound(throws.rock, throws.rock), results.draw);
+    expect.equal(scoreRound(throws.rock, throws.paper), results.computerWin);
+    expect.equal(scoreRound(throws.rock, throws.scissors), results.playerWin);
 
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+    expect.equal(scoreRound(throws.paper, throws.rock), results.playerWin);
+    expect.equal(scoreRound(throws.paper, throws.paper), results.draw);
+    expect.equal(scoreRound(throws.paper, throws.scissors), results.computerWin);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(scoreRound(throws.scissors, throws.rock), results.computerWin);
+    expect.equal(scoreRound(throws.scissors, throws.paper), results.playerWin);
+    expect.equal(scoreRound(throws.scissors, throws.scissors), results.draw);
+});
+
+test('rock paper scissors random throw', (expect) => {
+    const set = new Set();
+    for (let i = 0; i < 100; i++) {
+        set.add(getRandomThrow());
+    }
+
+    // Assert that we've only seen three values
+    expect.equal(set.size, 3);
+
+    // Assert that we've only seen *our* three values
+    expect.equal(set.has(throws.rock), true);
+    expect.equal(set.has(throws.paper), true);
+    expect.equal(set.has(throws.scissors), true);
+
+    // TODO: Test that we've got a flat distribution
+});
+
+test('throws and results immutability', (expect) => {
+    let mutatedRock = throws.rock;
+    mutatedRock += 'oops';
+    expect.notEqual(throws.rock, mutatedRock);
+
+    let mutatedDraw = results.draw;
+    mutatedDraw += 'did it again';
+    expect.notEqual(results.draw, mutatedDraw);
 });
